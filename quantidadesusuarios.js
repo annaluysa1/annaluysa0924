2fe9a2eb18c2870a9beece7caa66e0f22ef7ea89
annaluysa1 <136329533+annaluysa1@users.noreply.github.com>
2024-10-24 20:13:16 -0300

    Add files via upload

---
+++ quantidadesusuarios.js
@@ -0,0 +1,7 @@
+async function quantidadeUsuarios () {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+      
+      console.log(dados)
+}
